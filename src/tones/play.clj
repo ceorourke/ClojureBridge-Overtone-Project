80faeb01b878218477859893fa14432245afe050
colleen <elena.colleen@gmail.com>
2017-09-18 12:59:47 -0700

    Playing with Overtone

--- src/tones/play.clj
+++ src/tones/play.clj
@@ -1,5 +1,5 @@
 (ns tones.play
-  (:use [clojure.repl]
+  (:use ;[clojure.repl]
         [tones.twinkle]
         [overtone.live]
         [overtone.inst.piano]))
@@ -15,9 +15,27 @@
 ;; evaluate each form (line or function) by hitting
 ;;   ctrl + enter, or
 ;;   cmd + enter
-;;
+;;(demo (pan2 (sin-osc 220)))
 ;; for example
 ;; (piano 67)
 ;;
 ;; listen to the sound you have made
 ;;
+(defn piano-chord
+  [root chord-name]
+  (doseq [note (chord root chord-name)]
+    (piano note)))
+
+(defn up-above
+  [start]
+  (let [step 650
+        notes [:g3 :g3 :f3 :f3 :e3 :e3 :d3]]
+    (dotimes [i (count notes)]
+      (at (+ start (* i step)) (piano (note (nth notes i)))))))
+
+(let [start (now)]
+  (twinkle start)
+  (up-above (+ start 10400))
+  (up-above (+ start 15600))
+  (twinkle (+ start 20800)))
+
